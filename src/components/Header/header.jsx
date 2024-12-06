2a293ca9a1643e8299ed7dfa4c4eb2296dabdf97
Aaditya <aadityarevandkar#gmail.com>
2024-12-06 22:49:48 +0530

    6Dec

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import './header.sass'
+import React from "react";
+import "./header.css";
 
 const header = () => {
   return (
-    <div>header</div>
-  )
-}
+    <>
+      <header>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-2"></div>
+            <div className="col-sm-5">
+              <div className="headerSearch d-flex align-items-center"></div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
 
-export default header
+export default header;
