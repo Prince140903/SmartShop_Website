e0fe566cf1ca69f77e109e46e7c85d6e09b4250f
Akshay <akshaysonawane2510@gmail.com>
2024-12-09 15:52:22 +0530

    add some new things in nav but it need some content in header.js . small changes in app.css for colour

--- src/components/Header/nav/nav.js
+++ src/components/Header/nav/nav.js
@@ -4,7 +4,24 @@ import './nav.css';
 const Nav =() =>{ 
     return(
         <div className = "nav">
-            <div className='container-fluid' >
+            <div className='container-fluid d-flex' >
+                <div className='row'>
+                    <div className='col-sm-3 part 1'>
+
+
+                        </div>
+                    
+                             <div className='col-sm-7 part 2'>
+                                <nav>
+                                     //want button which   is used in header.js
+                                </nav>
+
+                        </div>
+                         <div className='col-sm-2 part 3'>
+
+                        </div>
+
+                </div>
             
             </div>
         </div>
