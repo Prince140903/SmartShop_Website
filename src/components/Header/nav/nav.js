18873236e92b305f24e5afd9ae6d6a683c21cab5
Akshay <akshaysonawane2510@gmail.com>
2024-12-16 00:16:21 +0530

    working with nav add some new elemnt and pages

--- src/components/Header/nav/nav.js
+++ src/components/Header/nav/nav.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './nav.css';
-
+import Button from '@mui/material/Button';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDownIcon';
 const Nav =() =>{ 
     return(
-        <div className = "nav">
-            <div className='container-fluid d-flex' >
+        <div className = 'nav d-flex align-items-center'>
+            <div className='container-fluid ' >
                 <div className='row'>
                     <div className='col-sm-3 part 1'>
 
@@ -13,6 +14,7 @@ const Nav =() =>{
                     
                              <div className='col-sm-7 part 2'>
                                 <nav>
+                                    <button className='bg-g text-white catTab'>browse all categories <KeyboardArrowDownIcon/> </button>
                                      //want button which   is used in header.js
                                 </nav>
 
