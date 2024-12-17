edc59becfe080fa20ba828c6a717ada49ef060a9
Akshay <akshaysonawane2510@gmail.com>
2024-12-17 23:40:44 +0530

    navigation  of header compelet . now sub menu will starting

--- src/components/Header/nav/nav.js
+++ src/components/Header/nav/nav.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDownIcon';
 import GridviewIcon from '@mui/icons-material/Gridview';
+import HeadphonesIcon from '@mui/icons-material/Headphones';
 const Nav =() =>{ 
     return(
         <div className = 'nav d-flex align-items-center'>
@@ -32,13 +33,19 @@ const Nav =() =>{
                                     <Button><Link>Vendors</Link></Button>
                                 </li>
                                   <li className='list list-inline'> 
-                                    <Button><Link>Mega Menu</Link></Button>
+                                    <Button><Link>Mega Menu <KeyboardArrowDownIcon /> </Link></Button>
                                 </li>
                                   <li className='list list-inline'> 
                                     <Button><Link>Blog</Link></Button>
                                 </li>
                                  <li className='list list-inline'> 
-                                    <Button><Link>pages</Link></Button>
+                                  <Button><Link>pages<KeyboardArrowDownIcon /> </Link></Button>
+                                  <div className='dropdown_menu'>
+                                    <ul>
+                        
+
+                                    </ul>
+                                  </div>
                                 </li>
                                  <li className='list list-inline'> 
                                     <Button><Link>Contact</Link></Button>
@@ -47,14 +54,22 @@ const Nav =() =>{
                          </nav>
 
                         </div>
-                         <div className='col-sm-2 part 3'>
+                         <div className='col-sm-2 part 3 d-flrx align-items-center'>
+                            <div className='phNo  d-flrx align-items-center ml-auto'>
+                            </div>
+                              <span>HeadphonesOutlinedIcon/</span>
+                              <div className='info ml-3'>
+                                <h3 className='text-g'>9100-9100</h3>
+                                <p className='mb-0'>24/7 Support center</p>
+                              </div>
+                          
 
                         </div>
 
                 </div>
             
-        //     </div>
-        // </div>
+               </div>
+           </div>
     )
 }
 
